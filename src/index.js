ca5abef5579043afc60be41c255f22ff4f9fcccb
Steve Steinitz <steve@dtctc.com>
2016-08-08 09:52:00 +1000

    split sass into files

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ import configureStore from './store/configureStore';
 import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-// import './styles/styles.css'; // Webpack can import CSS files too
-// require("./styles/styles.css");
-// require("!style!css!./styles/styles.css");
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/toastr/build/toastr.min.css';
