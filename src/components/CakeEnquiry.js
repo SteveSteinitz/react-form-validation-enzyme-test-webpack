341a3bd713c4a52efa3facebe8457e939f0ae353
Steve Steinitz <steve@dtctc.com>
2016-08-07 15:43:34 +1000

    Created presentation component: EnquiryForm and rendered it with dummy props.

--- src/components/CakeEnquiry.js
+++ src/components/CakeEnquiry.js
@@ -1,15 +1,40 @@
 // Created by steinitz on 07 Jul 2016
 
 import React from 'react';
+import EnquiryForm from './enquiryForm';
 
 class CakeEnquiry extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Cake Enquiry</h1>
-      </div>
-    );
-  }
+	cakeTypes ()
+	{
+		return ["Cupcakes", "Cheesecakes", "Butter Cakes", "Mudcakes"];
+	}
+	
+	celebrationTypes ()
+	{
+		return ["Birthday", "Wedding", "Corporate", "Other"];
+	}
+	
+	dummy ()
+	{}
+	
+	render () {
+		const enquiryFormProps = {
+			enquiry: {},
+			cakeTypes: this.cakeTypes (),
+			celebrationTypes: this.celebrationTypes(),
+			onSubmit: this.dummy,
+			onReset: this.dummy,
+			onChange: this.dummy,
+			onCelebrationTypeChange: this.dummy,
+			selectedCelebrationType: "Other",
+			errors: {}
+		};
+		return (
+			<div>
+				<EnquiryForm {...enquiryFormProps}/>
+			</div>
+		);
+	}
 }
 
 export default CakeEnquiry;
